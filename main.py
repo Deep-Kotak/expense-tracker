d2331aa571fc2c27bda0f2a8394a33d91bcb33cc
Deep-Kotak <deepmkotak1103@gmail.com>
2026-06-15 20:50:11 +0530

    Completed CRUD operations for expense tracker

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from database import connect_db
 
+
+def add_expense():
+    amount = float(input("Enter Amount: "))
+    category = input("Enter Category: ")
+    date = input("Enter Date (YYYY-MM-DD): ")
+
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO expenses(amount, category, expense_date) VALUES (?, ?, ?)",
+        (amount, category, date)
+    )
+
+    conn.commit()
+
+    print("Expense Added Successfully!")
+
+    conn.close()
+
+
 def view_expenses():
     conn = connect_db()
     cursor = conn.cursor()
@@ -16,7 +37,32 @@ def view_expenses():
 
     conn.close()
 
-from database import connect_db
+
+def update_expense():
+    expense_id = int(input("Enter Expense ID to Update: "))
+
+    amount = float(input("Enter New Amount: "))
+    category = input("Enter New Category: ")
+    date = input("Enter New Date (YYYY-MM-DD): ")
+
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE expenses
+        SET amount = ?, category = ?, expense_date = ?
+        WHERE id = ?
+    """, (amount, category, date, expense_id))
+
+    conn.commit()
+
+    if cursor.rowcount > 0:
+        print("Expense Updated Successfully!")
+    else:
+        print("Expense ID Not Found!")
+
+    conn.close()
+
 
 def delete_expense():
     expense_id = int(input("Enter Expense ID to Delete: "))
@@ -38,4 +84,33 @@ def delete_expense():
 
     conn.close()
 
-view_expenses()
+
+while True:
+
+    print("\n===== EXPENSE TRACKER =====")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Update Expense")
+    print("4. Delete Expense")
+    print("5. Exit")
+
+    choice = input("Enter Choice: ")
+
+    if choice == "1":
+        add_expense()
+
+    elif choice == "2":
+        view_expenses()
+
+    elif choice == "3":
+        update_expense()
+
+    elif choice == "4":
+        delete_expense()
+
+    elif choice == "5":
+        print("Good Bye!")
+        break
+
+    else:
+        print("Invalid Choice!")
